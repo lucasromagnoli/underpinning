83ac2c3d4de95246a822773f738cd3dd4cca6747
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2020-02-04 23:08:00 -0200

    Inserindo as annotações JPA e aperfeiçoando a classe do usuário sistema.

--- underpinning-domain/src/main/java/br/com/lucasromagnoli/javaee/underpinning/domain/model/SystemUser.java
+++ underpinning-domain/src/main/java/br/com/lucasromagnoli/javaee/underpinning/domain/model/SystemUser.java
@@ -1,16 +1,34 @@
 package br.com.lucasromagnoli.javaee.underpinning.domain.model;
 
+import javax.persistence.*;
 import java.util.List;
 
 /**
  * @author github.com/lucasromagnoli
  * @since 03/02/2020
  */
+@Entity
+@Table(name = "UND_SYSTEM_USER")
 public class SystemUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_SYSTEM_USER")
     private Long id;
+
+    @Column(name = "USERNAME")
     private String username;
+
+    @Column(name = "PASSWORD")
     private String password;
-    private List<String> roles;
+
+    @ManyToMany
+    @JoinTable(
+            name = "UND_SYSTEM_USER_ROLES",
+            joinColumns = @JoinColumn(name = "ID_SYSTEM_USER"),
+            inverseJoinColumns = @JoinColumn(name = "ID_SYSTEM_ROLES")
+    )
+    private List<SystemRole> roles;
 
     public Long getId() {
         return id;
@@ -36,11 +54,11 @@ public class SystemUser {
         this.password = password;
     }
 
-    public List<String> getRoles() {
+    public List<SystemRole> getRoles() {
         return roles;
     }
 
-    public void setRoles(List<String> roles) {
+    public void setRoles(List<SystemRole> roles) {
         this.roles = roles;
     }
 }
