5aa9e9263db3837ea31f51beb0aef67cd1224a3b
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2020-02-05 22:09:31 -0200

    Inserindo o model e service responsável por autenticar e gerar token

--- underpinning-web/src/main/java/br/com/lucasromagnoli/javaee/underpinning/rest/service/UnderpinningJwtSecurityService.java
+++ underpinning-web/src/main/java/br/com/lucasromagnoli/javaee/underpinning/rest/service/UnderpinningJwtSecurityService.java
@@ -1,9 +1,12 @@
 package br.com.lucasromagnoli.javaee.underpinning.rest.service;
 
 import br.com.lucasromagnoli.javaee.underpinning.domain.model.SystemUser;
+import br.com.lucasromagnoli.javaee.underpinning.domain.service.SystemUserService;
+import br.com.lucasromagnoli.javaee.underpinning.rest.model.JwtAppAuthorized;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -13,13 +16,21 @@ import java.security.PublicKey;
  * @since 03/02/2020
  */
 public class UnderpinningJwtSecurityService {
-    private final String TOKEN_PREFIX = "Bearer ";
+    private static final String TOKEN_PREFIX = "Bearer ";
     private PublicKey publicKey;
     private PrivateKey privateKey;
+
+    @Autowired
+    SystemUserService systemUserService;
 
     public UnderpinningJwtSecurityService(PublicKey publicKey, PrivateKey privateKey) {
         this.publicKey = publicKey;
         this.privateKey = privateKey;
+    }
+
+    public JwtAppAuthorized authenticateSystemUser(SystemUser systemUser) {
+        SystemUser userAuthenticated = systemUserService.findToAuthenticate(systemUser.getUsername(), systemUser.getPassword());
+        return new JwtAppAuthorized(createAuthorizationToken(userAuthenticated));
     }
 
     public String createAuthorizationToken(SystemUser systemUser) {
